811ddd948bc334c836f5f106897fbca8ce3d5a14
oceaann <47253870+oceaann@users.noreply.github.com>
2021-12-02 23:37:04 +0100

    Navbar component

---
+++ vue-app/components/navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav>
+    <div class="left"><NuxtLink to="/">Home</NuxtLink></div>
+    <div class="left"><NuxtLink to="/events">Events</NuxtLink></div>
+    <login class="login" />
+  </nav>
+</template>
+
+<style>
+
+  .left {
+    padding-right: 30px;
+  }
+
+  nav {
+    display: flex;
+    margin: 35px 76px;
+    height: 50px;
+    width: calc(100% - 152px);
+    justify-content: left;
+  }
+
+  a {
+    font-style: normal;
+    font-weight: normal;
+    font-size: 40px;
+    line-height: 47px;
+  }
+
+  .login {
+    margin-left: auto;
+  }
+
+</style>
--- vue-app/layouts/default.vue
+++ vue-app/layouts/default.vue
@@ -1,4 +1,5 @@
 <template>
+  <navbar />
   <slot />
 </template>
 
