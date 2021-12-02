0fed9f6c2e3f264474ce70933be97c9dc10852cd
oceaann <47253870+oceaann@users.noreply.github.com>
2021-12-03 00:21:25 +0100

    Login button

---
+++ vue-app/components/login.vue
@@ -0,0 +1,21 @@
+<template>
+  <a href="https://discord.com/api">
+    <div class="login-root">
+      <img src="~/assets/discord.png" />
+      <div>Login</div>
+    </div>
+  </a>
+</template>
+
+<style>
+  .login-root {
+    display: flex;
+  }
+
+  img {
+    height: 50px;
+    align-items: center;
+    margin-top: 2px;
+    margin-right: 10px;
+  }
+</style>
