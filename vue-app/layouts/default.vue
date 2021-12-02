8f708f18298377647440c2af9b368f0598330bac
oceaann <47253870+oceaann@users.noreply.github.com>
2021-12-02 22:53:19 +0100

    Default layout styles

--- vue-app/layouts/default.vue
+++ vue-app/layouts/default.vue
@@ -1,3 +1,15 @@
 <template>
   <slot />
 </template>
+
+<style>
+  * {
+    color: #F2F2F2;
+    text-decoration: none;
+    margin: 0;
+  }
+
+  body {
+    background-color: #1C1C1C;
+  }
+</style>
